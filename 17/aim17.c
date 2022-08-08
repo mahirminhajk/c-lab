9095b40a445cd9d35e6fd4a097c31ade753b39e3
mahirminhajk <kmahirminhaj@gmail.com>
2022-08-08 18:47:55 +0530

    Aug 8
    added 14, 15, 16, 17

---
+++ 17/aim17.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+void main()
+{
+    int l1, l2, lf, i, j, k, temp;
+    int a[100], b[100], c[200];
+
+    printf("Enter the size of first array: ");
+    scanf("%d", &l1);
+    printf("Enter the size of second array: ");
+    scanf("%d", &l2);
+    lf = l1 + l2;
+
+    printf("Enter the first array elements:\n ");
+    for (i = 0; i < l1; i++)
+    {
+        scanf("%d", &a[i]);
+        c[i] = a[i];
+    }
+    k = l1;
+    printf("Enter the second array elements:\n ");
+    for (i = 0; i < l2; i++)
+    {
+        scanf("%d", &b[i]);
+        c[k] = b[i];
+        k++;
+    }
+
+    printf("After merging a and b array: \n");
+    for (i = 0; i < lf; i++)
+    {
+        printf("%d\t", c[i]);
+    }
+
+    for (i = 0; i < lf; i++)
+    {
+        for (j = 0; j < lf - i; j++)
+        {
+            if (c[j] > c[j + 1])
+            {
+                temp = c[j];
+                c[j] = c[j + 1];
+                c[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\nAfter sorting \n");
+    for (i = 0; i < lf; i++)
+    {
+        printf("%d\t", c[i]);
+    }
+
+    printf("\nProgram exited\n");
+}
